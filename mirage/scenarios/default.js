37f8b0e60cafcb4fefc5854cf52e27ba34db319f
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-14 17:16:36 -0600

    Fixes Slack new line characters

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,10 +1,10 @@
 export default function(server) {
-​
-/*
+
+  /*
     Seed your development database using your factories.
     This data will not be loaded in your tests.
-*/
-​
+  */
+
   server.create('recipe', {ingredients: [1,2,3,4,5,6]});
   server.createList('ingredient', {recipe: 1});
 }
