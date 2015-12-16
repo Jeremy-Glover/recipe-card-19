2d04e7a4161a0abd9ceaccd947c4109ea2138f37
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-15 22:08:46 -0600

    Fixes failing test and adapter

--- tests/acceptance/recipe/recipe-card-test.js
+++ tests/acceptance/recipe/recipe-card-test.js
@@ -4,6 +4,9 @@ import moduleForAcceptance from 'recipe-card-19/tests/helpers/module-for-accepta
 moduleForAcceptance('Acceptance | recipe/index');
 
 test('visiting the homepage', function(assert) {
+  server.create('recipe', {ingredients: [1, 2, 3, 4, 5, 6]});
+  server.create('ingredient', {recipe: 1, quantity: 2, name: 'Chicken Livers', unit: 'pounds'});
+  server.createList('ingredient', 5, {recipe: 1});
   visit('/');
 
   andThen(function() {
@@ -13,7 +16,7 @@ test('visiting the homepage', function(assert) {
 });
 
 test('should be able to see list of 6 items', function(assert) {
-  server.createList('recipe', 1);
+  server.create('recipe', {ingredients: [1, 2, 3, 4, 5, 6]});
   server.create('ingredient', {recipe: 1, quantity: 2, name: 'Chicken Livers', unit: 'pounds'});
   server.createList('ingredient', 5, {recipe: 1});
   visit('/');
@@ -27,7 +30,7 @@ test('should be able to see list of 6 items', function(assert) {
 });
 
 test('should be able to change servings value', function(assert) {
-  server.createList('recipe', 1);
+  server.create('recipe', {ingredients: [1, 2, 3, 4, 5, 6]});
   server.create('ingredient', {recipe: 1, quantity: 2, name: 'Chicken Livers', unit: 'pounds'});
   server.createList('ingredient', 5, {recipe: 1});
   visit('/');
